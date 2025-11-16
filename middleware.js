a05e4e5f740984f92c0d037e1df22a8899134264
Ajaykumar995 <224g1a0502@srit.ac.in>
2025-11-16 14:01:11 +0530

    Fix Schema.js import path

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const Listing = require("./models/listing.js");
 const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema,reviewSchema } = require("./schema.js");
+const { listingSchema,reviewSchema } = require("./Schema.js");
 
 
 const isLoggedIn = (req, res, next) => {
