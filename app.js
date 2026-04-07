1fa4ebe40eda9f9e6eb6f0a8b950c4d26efceb8b
Ajaykumar995 <224g1a0502@srit.ac.in>
2026-04-07 16:14:14 +0530

    fix render port issue

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
+const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
 
@@ -37,6 +37,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req,res) => {
+res.redirect('/listings');
+});
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
@@ -67,15 +71,16 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+console.log(User);
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
     next();
 });
 
@@ -104,6 +109,8 @@ app.use((err, req, res, next) => {
     next();
 });
 
-app.listen(8080, () => {
-    console.log("App is listening.");
-})
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
